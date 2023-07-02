44de324f49b805a3947a2f898e6b1750944f03e4
AmirHossein-nr <arazlighi@gmail.com>
2023-07-02 15:22:10 +0330

    implement stack

---
+++ codegen/Stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if len(self.items) > 0:
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):
+        if len(self.items) > 0:
+            return self.items[len(self.items) - 1]
+        else:
+            return None
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def __str__(self):
+        return str(self.items)
