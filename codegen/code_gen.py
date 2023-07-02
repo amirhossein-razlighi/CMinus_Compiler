2e7e91162a165b9107497761ce91a39ca30b2995
AmirHossein-nr <arazlighi@gmail.com>
2023-07-02 15:34:40 +0330

    edit code_gen __init__

--- codegen/code_gen.py
+++ codegen/code_gen.py
@@ -1,4 +1,7 @@
 from typing import Optional
+from Stack import Stack
+from PB import ProgramBlock
+
 
 class Routines:
     instance: Optional[Routines] = None
@@ -8,5 +11,13 @@ class Routines:
             Routines.instance = Routines()
         return Routines.instance
 
-    def __init__(self, semantic_stack):
+    def __init__(self, semantic_stack: Stack, program_block: ProgramBlock):
         self.semantic_stack = semantic_stack
+        self.program_block = program_block
+
+    def push_id(self, id):
+        self.semantic_stack.push(id)
+
+    def assign(self):
+        source = self.semantic_stack.pop()
+        target = self.semantic_stack.pop()
